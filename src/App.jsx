4d3e7d79932cee4c677ee1c0497af7afd3107668
Roxylius <anandabhijeet786+github@gmail.com>
2025-09-09 16:13:14 +0530

    add: package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Menu, X } from "lucide-react";
 import { useState } from "react";
 import logo from "../assets/logo.png";
