b11b7f4e0b8501e227b3fb21233d8db899626d8f
Roxylius <anandabhijeet786+github@gmail.com>
2025-09-12 14:25:39 +0530

    gh pages changes

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/', // This is crucial!
+  base: process.env.NODE_ENV === 'production' ? '/landingPage/' : '/',
   // An array of plugins to use.
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   // Development server configuration.
   server: {
+    open: true,
     // Hosts that are allowed to access the dev server. for porting
     allowedHosts: [
       'fd76a726e099.ngrok-free.app'
